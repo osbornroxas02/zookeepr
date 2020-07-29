a1b80146fbd45dadb6bdf54c9bb11945faad568f
osborn roxas <osbornroxas02@gmail.com>
2020-07-29 03:22:54 -0700

    updated port

--- server.js
+++ server.js
@@ -46,14 +46,22 @@ function filterByQuery(query, animalsArray) {
 }
 
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
+
 //used to chain on methods to the Express.js server
-app.get('/api/animals', (req, res) => {
-    let results = animals;
-    if (req.query) {
-        results = filterByQuery(req.query, results);
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
     }
-    res.json(results);
 });
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
